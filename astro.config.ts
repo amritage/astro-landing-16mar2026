d1bf2d87ef1f3aee50dbd8eaa14111d7788c9974
Madhu Pandey <madhupandey29052004@gmail.com>
2026-04-14 13:46:36 +0530

    fix

--- astro.config.ts
+++ astro.config.ts
@@ -8,6 +8,12 @@ import { cloudinaryPicture } from './src/integrations/cloudinary-picture.ts';
 const redirects = await buildAstroRedirects();
 
 export default defineConfig({
+  trailingSlash: 'never',
+  output: 'static',
+  prefetch: {
+    prefetchAll: true,
+    defaultStrategy: 'viewport',
+  },
   redirects,
   site: process.env.PUBLIC_SITE_URL ?? 'http://localhost:4321',
   integrations: [mdx(), cloudinaryPicture(), partytown({ config: { forward: ['dataLayer.push', 'gtag', 'clarity'] } })],
