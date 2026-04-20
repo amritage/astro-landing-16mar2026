cfb33ef9f59fb62ac5d80d281822e75f9270e1bd
Rajesh Goyal <AGE.utilities@gmail.com>
2026-04-20 20:01:34 +0530

    rwe
    test

--- astro.config.ts
+++ astro.config.ts
@@ -18,7 +18,6 @@ export default defineConfig({
   site: process.env.PUBLIC_SITE_URL ?? 'http://localhost:4321',
   integrations: [mdx(), cloudinaryPicture(), partytown({ config: { forward: ['dataLayer.push', 'gtag', 'clarity'] } })],
   build: {
-    format: 'file',
     inlineStylesheets: 'always',
   },
   vite: {
